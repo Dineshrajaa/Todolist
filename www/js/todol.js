0ad26d47d628a5dc609aa92e656133f9d42c0d9c
Dineshrajaa <dineshrajaa.93@gmail.com>
2014-12-02 16:35:22 +0530

    Fixed few bugs

--- www/js/todol.js
+++ www/js/todol.js
@@ -10,8 +10,10 @@ function addToDo(){
                     tx.executeSql("insert into todotable(title,desc,schedule_date,schedule_time) values(?,?,?,?)",[titl,description,sch_date,sch_time]);
                     //alert("Inserted Successfully");
                 });
+
             }
             else alert("Enter all the data");
+            
         }
         //Function to Display Todo's in Listview
         function showToDo(transaction,results){
@@ -21,6 +23,7 @@ function addToDo(){
             }
 
                 $("#todolist").listview("refresh");
+                
         }
         //Function to read Todo's from DB
         function readToDo(){
@@ -30,4 +33,20 @@ function addToDo(){
                 tx.executeSql("select * from todotable",[],showToDo);
 
             });
+        }
+
+        function fullyLoaded(){
+            if (window.openDatabase) {
+                var db=window.openDatabase("ToDo",1.0,"ToDo",5242880);
+                db.transaction(function(tx){
+                    //tx.executeSql("drop table todotable");
+                    tx.executeSql("create table if not exists todotable(todoid integer primary key asc,title text,desc text,schedule_date text,schedule_time text)");
+                    //alert("Table Created");
+                    readToDo();
+                });
+            }
+            else{
+                alert("Your Device donot support offline storage");
+            }
+            
         }
